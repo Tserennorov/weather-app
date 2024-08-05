85cbced3689f74ce529a8fbbd8761556b6b461a8
24LP1665 24LP1665 <24LP1665@G302-CC0154.local>
2024-08-05 12:47:54 +0800

    weather-app

---
+++ first.js
@@ -0,0 +1,37 @@
+const weathers = [
+  {
+    city: "New York",
+    temperature: 28,
+    weather: "Sunny",
+    icon: "sun",
+    day: "day",
+  },
+  {
+    city: "London",
+    temperature: 18,
+    weather: "Cloudy",
+    icon: "cloudy",
+    day: "night",
+  },
+  {
+    city: "Tokyo",
+    temperature: 30,
+    weather: "Rainy",
+    icon: "rainy",
+    day: "day",
+  },
+  {
+    city: "Sydney",
+    temperature: 22,
+    weather: "Partly Cloudy",
+    icon: "cloudy",
+    day: "day",
+  },
+  {
+    city: "Paris",
+    temperature: 25,
+    weather: "Clear",
+    icon: "moon",
+    day: "night",
+  },
+];
